090e4f21beed700506e259de6e60f89469bd1edf
Paul Johnson <paul.johnson@touchstar.co.uk>
2025-07-22 16:58:15 +0100

    Add Triple and QuadTupleMessage classes

---
+++ Soduku/Models/TupleMessage.cs
@@ -0,0 +1,26 @@
+﻿namespace Soduku.Models
+{
+    public class DoubleTupleMessage
+    {
+#nullable enable
+        public Tuple<object, object>? Message { get; set; }
+#nullable disable
+        public string Sender { get; set; } = "";
+    }
+
+    public class TripleTupleMessage
+    {
+#nullable enable
+        public Tuple<object, object, object>? Message { get; set; }
+#nullable disable
+        public string Sender { get; set; } = "";
+    }
+
+    public class QuadTupleMessage
+    {
+#nullable enable
+        public Tuple<object, object, object, object>? Message { get; set; }
+#nullable disable
+        public string Sender { get; set; } = "";
+    }
+}
