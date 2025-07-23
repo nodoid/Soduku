7aa5d7ccb288242a0a129b93172c53e65d0270b3
Paul Johnson <paul.johnson@touchstar.co.uk>
2025-07-23 12:04:09 +0100

    Set to landscape mode

--- Soduku/Platforms/Android/MainActivity.cs
+++ Soduku/Platforms/Android/MainActivity.cs
@@ -1,10 +1,10 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace Sudoku
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density
+        | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
